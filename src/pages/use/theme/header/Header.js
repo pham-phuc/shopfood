19c2cb01ecb9c7c1d311fa0a83ef04abd19c21ef
pham-phuc <101715209+pham-phuc@users.noreply.github.com>
2023-09-19 22:15:56 +0700

    fist commit

--- src/pages/use/theme/header/Header.js
+++ src/pages/use/theme/header/Header.js
@@ -1,4 +1,4 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import "./style.scss";
 import {
   AiFillFacebook,
@@ -7,49 +7,129 @@ import {
   AiFillLinkedin,
   AiOutlineUser,
   AiOutlineMail,
+  AiOutlineShoppingCart,
 } from "react-icons/ai";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import { formater } from "ultils/fomater";
+import { ROUTERS } from "ultils/router";
 const Header = () => {
+  const [menus, setMenus] = useState([
+    {
+      name: "HOME",
+      path: ROUTERS.USER.HOME,
+    },
+    {
+      name: "SHOP",
+      path: ROUTERS.USER.SHOP,
+    },
+    {
+      name: "PRODUCT",
+      path: ROUTERS.USER.PRODUCTS,
+      isShowSubmenu: false,
+      child: [
+        {
+          name: "MEAT",
+          path: "",
+        },
+        {
+          name: "VEGETABLE",
+          path: "",
+        },
+        {
+          name: "FAST FOOD",
+          path: "",
+        },
+      ],
+    },
+    {
+      name: "BLOG",
+      path: ROUTERS.USER.HOME,
+    },
+    {
+      name: "CONTACT",
+      path: ROUTERS.USER.HOME,
+    },
+  ]);
+
   return (
-    <div className="header-top">
+    <div>
+      <div className="header-top">
+        <div className="container">
+          <div className="row">
+            <div className="col-6 header-top-left">
+              <ul>
+                <li>
+                  <AiOutlineMail />
+                  <span>Foodshop@gmail.com</span>
+                </li>
+                <li>Miễn phí ship đơn từ {formater(200000)}.</li>
+              </ul>
+            </div>
+            <div className="col-6 header-top-right">
+              <ul>
+                <li>
+                  <Link to={""}>
+                    <AiFillFacebook />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <AiFillInstagram />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <AiOutlineTwitter />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <AiFillLinkedin />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <AiOutlineUser />
+                  </Link>
+                  <span>Login</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="container">
         <div className="row">
-          <div className="col-6 header-top-left">
-          <ul>
-            <li><AiOutlineMail/><span>Foodshop@gmail.com</span></li>
-            <li>Miễn phí ship đơn từ {formater(200000)}.</li>
-          </ul>
+          <div className="col-xl-3">
+            <div className="header-logo">
+              <h1>ShopFood</h1>
+            </div>
           </div>
-          <div className="col-6 header-top-right">
-            <ul>
-              <li>
-                <Link to={""}>
-                  <AiFillFacebook />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <AiFillInstagram />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <AiOutlineTwitter />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <AiFillLinkedin />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <AiOutlineUser />
-                </Link>
-                <span>Login</span>
-              </li>
-            </ul>
+          <div className="col-xl-6">
+            <div className="header-menu">
+              <ul>
+                {menus?.map((menu, menuKey) => (
+                  <li key={menuKey} className={menuKey === 0 ? "active" : ""}>
+                    <Link to={menu?.path}>{menu?.name}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className="col-xl-3">
+            <div className="header-cart">
+              <div className="header-cart-price">
+                <span>{formater(1200000)}</span>
+              </div>
+              <ul>
+                <li>
+                  <Link to="#">
+                    <AiOutlineShoppingCart />
+                    <span>5</span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
--- src/ultils/router.js
+++ src/ultils/router.js
@@ -1,6 +1,8 @@
 export const ROUTERS = {
-    USER:{
-        HOME: "",
-        PROFILE:"ttcn",
-    }
-}
+  USER: {
+    HOME: "",
+    PROFILE: "ttcn",
+    SHOP: "shop",
+    PRODUCTS: "/san-pham",
+  },
+};
